fde8a6fa4b71991a24b956c6d0a8e72688b02fd7
weijiazhang616 <weijiazhang616@gmail.com>
2019-01-29 22:33:40 -0500

    set second file

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/js/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+module.exports = [
+    {
+        entry: './src/js/index.js',
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'compiled_js')
+        }
+    },
+    {
+        entry: './src/js/ES6_Promise.js',
+        output: {
+            filename: 'es6_promise_compiled.js',
+            path: path.resolve(__dirname, 'compiled_js')
+        }
     }
-};
+];

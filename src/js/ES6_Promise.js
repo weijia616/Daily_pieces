bbafb0d357f2452e30924cdfc25104ea05ca1dde
weijiazhang616 <weijiazhang616@gmail.com>
2019-01-30 23:29:43 -0500

    promise

--- src/js/ES6_Promise.js
+++ src/js/ES6_Promise.js
@@ -22,3 +22,17 @@
     });
     console.log('Hi');
 }
+
+/**
+ * 调用resolve(1)以后，后面的console.log(2)还是会执行，并且会首先打印出来。
+ * 这是因为立即 resolved 的 Promise 是在本轮事件循环的末尾执行，总是晚于本轮循环的同步任务。
+ * 所以输出为 2 -- 1
+ */
+{
+    new Promise((resolve, reject) => {
+        resolve(1);
+        console.log(2);
+    }).then(r => {
+        console.log(r);
+    })
+}

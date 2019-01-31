d287edfff43384a1f3672ae406e1b5dbed26c912
weijiazhang616 <weijiazhang616@gmail.com>
2019-01-30 23:31:56 -0500

    promise

--- src/js/ES6_Promise.js
+++ src/js/ES6_Promise.js
@@ -36,3 +36,16 @@
         console.log(r);
     })
 }
+
+/**
+ * 一般来说，调用resolve或reject以后，Promise 的使命就完成了，
+ * 后继操作应该放到then方法里面，而不应该直接写在resolve或reject的后面。
+ * 所以，最好在它们前面加上return语句
+ */
+{
+    new Promise((resolve, reject) => {
+        return resolve(1);
+        // 后面的语句不会执行
+        console.log(2);
+    })
+}

eecd28b7b6cf0883681b84cf5372d9196f27e9a1
weijiazhang616 <weijiazhang616@gmail.com>
2019-02-14 23:42:52 -0500

    continue Promise ts

--- src/js/ES6_Promise.ts
+++ src/js/ES6_Promise.ts
@@ -55,3 +55,18 @@
 }
 
 
+/**
+ * 我们可以将图片的加载写成一个Promise，一旦加载完成，Promise的状态就发生变化
+ */
+{
+    const preloadImage = (path) => {
+        return new Promise(((resolve, reject) => {
+            const image = new Image();
+            image.onload = resolve;
+            image.onerror = reject;
+            image.src = path;
+        }))
+    }
+}
+
+

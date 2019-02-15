e2189f9e1173f19f294f2f7a8db087fc61f740ed
weijiazhang616 <weijiazhang616@gmail.com>
2019-02-14 23:23:58 -0500

    continue Promise ts

--- src/js/ES6_Promise.ts
+++ src/js/ES6_Promise.ts
@@ -28,4 +28,30 @@
     console.log('Hi');
 }
 
+/**
+ * Promise.all方法用于将多个 Promise 实例，包装成一个新的 Promise 实例。
+ * 当所有的promise 都有结果返回了， 新的Promise.all才会调用回调函数
+ * 如果作为参数的 Promise 实例，自己定义了catch方法，那么它一旦被rejected，并不会触发Promise.all()的catch方法。
+ * 如果p2没有自己的catch方法，就会调用Promise.all()的catch方法。
+ * Output: ['hello', 'p2 has Error']
+ */
+{
+    const p1 = new Promise(((resolve, reject) => {
+        resolve('hello');
+    }))
+        .then(result => result)
+        .catch(e => e);
+
+    const p2 = new Promise(((resolve, reject) => {
+        throw new Error('p2 has Error');
+    }))
+        .then(result => result)
+        .catch(e => e);
+
+
+    Promise.all([p1, p2])
+        .then(result => console.log(result))
+        .catch(e => console.log(e));
+}
+
 

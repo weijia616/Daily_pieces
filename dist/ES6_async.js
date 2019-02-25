0b82d06e057abe84a87926d22ec5baf731c5dc11
weijiazhang616 <weijiazhang616@gmail.com>
2019-02-24 23:28:13 -0500

    sick and rest

--- dist/ES6_async.js
+++ dist/ES6_async.js
@@ -1,2 +1,34 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 {
+    function timeout(ms) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield new Promise((resolve => {
+                setTimeout(resolve, ms);
+            }));
+        });
+    }
+    function asyncConsoleLog(string, ms) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield timeout(ms);
+            console.log(string);
+        });
+    }
+    asyncConsoleLog('hello world after 50ms', 50);
+}
+{
+    function getTitle(url) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let response = yield fetch(url);
+            let html = yield response.text();
+            return html.match(/<title>([\s\S]+)<\/title>/i)[1];
+        });
+    }
+    getTitle('https://tc39.github.io/ecma262/').then(console.log);
 }
--- src/js/ES6_async.ts
+++ src/js/ES6_async.ts
@@ -1,4 +1,24 @@
 {
+    async function timeout(ms) {
+        await new Promise((resolve => {
+            setTimeout(resolve, ms);
+        }))
+    }
 
+    async function asyncConsoleLog(string, ms) {
+        await timeout(ms);
+        console.log(string);
+    }
 
+    asyncConsoleLog('hello world after 50ms', 50);
+}
+
+{
+    async function getTitle(url) {
+        let response = await fetch(url);
+        let html = await response.text();
+        return html.match(/<title>([\s\S]+)<\/title>/i)[1];
+    }
+
+    getTitle('https://tc39.github.io/ecma262/').then(console.log);
 }
